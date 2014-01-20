00209bb5ecca330d43cd3be828ab8a06173ec271
oxana.ryashentseva <oxana.ryashentseva@siemens.com>
2014-01-20 13:52:55 +0100

    changed repositories names to the correct ones and added one more repository (OMB-comove)

--- git-clone-all.sh
+++ git-clone-all.sh
@@ -2,10 +2,12 @@
 cd ..
 git clone https://github.com/imp-partner-test/IMP-North-Ideas.git
 git clone https://github.com/imp-partner-test/IMP-North-Ready.git
-git clone https://github.com/imp-partner-test/OMB-Verkehrsmittelfergleich.git
+git clone https://github.com/imp-partner-test/OMB-verkehrsmittelvergleich.git
 git clone https://github.com/imp-partner-test/OMB-tom-jell.git
-git clone https://github.com/imp-partner-test/OMB-Bosch.git
+git clone https://github.com/imp-partner-test/OMB-bosch.git
 git clone https://github.com/imp-partner-test/OMB-bmw.git
 git clone https://github.com/imp-partner-test/OMB-daimler.git
 git clone https://github.com/imp-partner-test/OMB-volkswagen.git
+git clone https://github.com/imp-partner-test/OMB-comove.git
+
 cd IMP-North-Implemente
