31c7dea7b2747c50b7bf86e122f01c7e62bad9dc
oxikproxik <oxana.ryashentseva@siemens.com>
2014-10-31 13:44:04 +0100

    m

--- git-clone-all.sh
+++ git-clone-all.sh
@@ -2,12 +2,9 @@
 #git clone https://github.com/imp-partner-test/IMP-North-Implemented.git
 git clone https://github.com/imp-partner-test/IMP-North-Implemented-DreddTestable.git
 git clone https://github.com/imp-partner-test/IMP-RSH.git
-git clone https://github.com/imp-partner-test/OMB-tom-jell.git
 git clone https://github.com/imp-partner-test/OMB-bosch.git
 git clone https://github.com/imp-partner-test/OMB-bmw.git
-git clone https://github.com/imp-partner-test/OMB-volkswagen.git
 git clone https://github.com/imp-partner-test/OMB-hacon.git
 git clone https://github.com/imp-partner-test/OMB-cityintel.git
-git clone https://github.com/imp-partner-test/OMB-parku.git
 git clone https://github.com/imp-partner-test/OMB-virtualcity.git
 git clone https://github.com/imp-partner-test/drivenow.git
